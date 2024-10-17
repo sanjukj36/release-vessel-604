81eff0e2d4d4a3a4c44fa10a0e4e6bf842bc85ff
Yadhukrishna CU <yadhukrishna.unnikrishnan@memphis-marine.com>
2024-10-17 17:57:16 +0530

    feat: add alert constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,20 @@
+// Api URI for application data.
 export const API = "http://192.168.18.64:8002";
+// Api URI for Inf-modules.
 export const API_INF = "http://192.168.18.99:3000";
 
+// Page refresh time.
 export const REFRESH_TIME = 1000 * 6;
+
+// Alerts.
+export const BANDWIDTH_ALARM = "Internet Connection Error.";
+export const PING_MDC_ALARM = "MDC Connection Lost.";
+export const PING_PLC_ALARM = "PLC Connection Lost.";
+export const PING_TRANSBOX_ALARM = "TRANSBOX Connection Lost.";
+export const FILE_SEND_STATUS = "Last 10 file submission failed.";
+
+// Database File
+export const FILE_STATUS = {
+  notYetProcessed: "Not Yet Processed ",
+  success: "Success"
+};
