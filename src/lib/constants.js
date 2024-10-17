92466b1cf43a0a4585ee25a1812fcd47d4c04038
Yadhukrishna CU <yadhukrishna.unnikrishnan@memphis-marine.com>
2024-10-17 15:03:55 +0530

    feat: bandwidth api setup

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,3 +1,4 @@
 export const API = "http://192.168.18.64:8002";
+export const API_INF = "http://192.168.18.99:3000";
 
 export const REFRESH_TIME = 1000 * 6;
--- src/pages/inf-module/telemetry-equipment-status/index.jsx
+++ src/pages/inf-module/telemetry-equipment-status/index.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { PageWrapper } from "@/components/layout/page-wrapper";
 import { Title } from "@/components/layout/title";
 import { RESPONSE_TES } from "@/dummy/responseTelemetryEquipmenmtStatus";
+import { getBandWidthStatusAPI } from "@/infrastructure/inf-module/all-api";
 import { BandwidthComponent } from "./bandwidth-component";
 import { DiskUsageComponent } from "./disk-usage-component";
 import { NetworkPingStatus } from "./network-status";
 
 function TelemetryEquipmentStatus(props) {
+  useEffect(() => {
+    fetchBandWidthData();
+  }, []);
+
+  const fetchBandWidthData = async () => {
+    const [data, err] = await getBandWidthStatusAPI();
+    if (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <PageWrapper className="space-y-2">
       <Title className="col-span-4" title="TELEMETRY EQUIPMENT STATUS" />
