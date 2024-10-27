4a4c94538ab6e367d26101958fe6c17954385cd8
Yadhukrishna CU <yadhukrishna.unnikrishnan@memphis-marine.com>
2024-10-27 16:55:57 +0530

    feat: add dynamic change with min and max params

--- src/components/ui/progress.jsx
+++ src/components/ui/progress.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { normalizeValue } from "@/lib/progressbar-utils";
 import { cn } from "@/lib/utils";
 
 const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
@@ -21,21 +22,24 @@ const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 const ProgressVertical = React.forwardRef(
-  ({ className, value, ...props }, ref) => (
-    <ProgressPrimitive.Root
-      ref={ref}
-      className={cn(
-        "relative h-full w-2 overflow-hidden rounded-full bg-primary/20",
-        className
-      )}
-      {...props}
-    >
-      <ProgressPrimitive.Indicator
-        className="h-full w-full flex-1 bg-primary transition-all"
-        style={{ transform: `translateY(${100 - (value || 0)}%)` }}
-      />
-    </ProgressPrimitive.Root>
-  )
+  ({ className, value = 50, min = 0, max = 100, ...props }, ref) => {
+    const progressValue = normalizeValue(min, max, value);
+    return (
+      <ProgressPrimitive.Root
+        ref={ref}
+        className={cn(
+          "relative h-full w-2 overflow-hidden rounded-full bg-primary/20",
+          className
+        )}
+        {...props}
+      >
+        <ProgressPrimitive.Indicator
+          className="h-full w-full flex-1 bg-primary transition-all"
+          style={{ transform: `translateY(${100 - (progressValue || 0)}%)` }}
+        />
+      </ProgressPrimitive.Root>
+    );
+  }
 );
 
 ProgressVertical.displayName = ProgressPrimitive.Root.displayName;
---
+++ src/lib/progressbar-utils.js
@@ -0,0 +1,16 @@
+/**
+ * Normalize a value to a scale of 0-100 based on the min and max range, allowing for negative values.
+ * @param {number} min - The minimum possible value.
+ * @param {number} max - The maximum possible value.
+ * @param {number} value - The current value to normalize.
+ * @returns {number} - A value between 0 and 100.
+ */
+export const normalizeValue = (min, max, value) => {
+  if (min >= max) {
+    throw new Error("Min should be less than Max.");
+  }
+
+  const boundedValue = Math.min(Math.max(value, min), max);
+
+  return ((boundedValue - min) / (max - min)) * 100;
+};
