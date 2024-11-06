2b775a723694aa28e1ef1975fc86439553f85be3
Yadhukrishna CU <yadhukrishna.unnikrishnan@memphis-marine.com>
2024-11-06 11:31:31 +0530

    feat: pwa integration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,60 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: [
+        "favicon.ico",
+        "android-chrome-192x192.png",
+        "android-chrome-512x512.png",
+        "apple-touch-icon.png",
+        "maskable-icon.png"
+      ],
+      manifest: {
+        name: "MQTT Live Data",
+        short_name: "MQTT",
+        description:
+          "MQTT Live Data Viewing and exporting in Excell Application",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png"
+          },
+          {
+            src: "/maskable-icon.png",
+            sizes: "225x225",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ],
+        theme_color: "#171717",
+        background_color: "#e8ebf2",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "landscape"
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024 // Increase cache size limit to 3 MB
+      }
+    })
+  ],
   server: {
     host: "192.168.18.99",
     port: 3013
@@ -12,6 +63,19 @@ export default defineConfig({
     alias: {
       // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src")
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            console.log({ id });
+            return "vendor";
+          }
+        }
+      }
     }
   }
 });
