fe9e2f7130e9ae6007fb084b2d2d7dca185deca9
Sanju K Joseph <sanju.joseph@mariapps.com>
2025-08-14 17:44:22 +0530

    fix:remove menu

--- src/components/partials/header/index.jsx
+++ src/components/partials/header/index.jsx
@@ -15,12 +15,12 @@ export function Header() {
   return (
     <div className="w-full z-10 py-0.5 flex gap-2 shadow-[0_0_5px_1px_rgba(0,0,0,0.5)] bg-accent h-[55px]">
       <div className="basis-1 flex-grow h-full flex items-center p-0.5">
-        <div
+        {/* <div
           onClick={handleClick}
           className="hover:bg-secondary transition-colors hover:opacity-100"
         >
           <img className="w-[40px] cursor-pointer" src={iconMenu} />
-        </div>
+        </div> */}
       </div>
       <div className="basis-1 flex-grow ml-auto p-0.5">
         <AlertNotificationMenu />
